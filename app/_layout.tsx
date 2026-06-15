6d9da8bf5b4c7c4fca8413ed5e65c4a68e5fa4f7
juliaht15 <julia.huertas@alu.ceac.fp.es>
2026-06-15 21:13:24 +0200

    layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,36 @@
+// E:\Proyectos\noteflow\app\_layout.tsx
+import { useEffect, useState } from 'react';
 import { Stack } from 'expo-router';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../lib/firebaseConfig';
+import { useRouter, useSegments } from 'expo-router';
 
 export default function RootLayout() {
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState<any>(null);
+  const router = useRouter();
+  const segments = useSegments();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (initializing) setInitializing(false);
+    });
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (initializing) return;
+
+    const inTabsGroup = segments[0] === '(tabs)';
+
+    if (!user && !inTabsGroup && segments[0] !== 'register') {
+      router.replace('/register');
+    } else if (user && segments[0] !== '(tabs)') {
+      router.replace('/(tabs)');
+    }
+  }, [user, initializing, segments]);
+
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="(tabs)" />
