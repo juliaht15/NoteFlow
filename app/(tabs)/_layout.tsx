bb42a775fb0f8703ccdf06cdd6b24218f3ea76ed
juliaht15 <julia.huertas@alu.ceac.fp.es>
2026-06-15 14:20:27 +0200

    components2

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,10 @@ import { Ionicons } from '@expo/vector-icons';
 export default function TabsLayout() {
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: 'blue', headerShown: true }}>
-      <Tabs.Screen name="index" options={{ title: 'Inicio', tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} /> }} />
-      <Tabs.Screen name="notas" options={{ title: 'Notas', tabBarIcon: ({ color }) => <Ionicons name="document-text" size={24} color={color} /> }} />
-      <Tabs.Screen name="checklists" options={{ title: 'Checklist', tabBarIcon: ({ color }) => <Ionicons name="checkbox" size={24} color={color} /> }} />
-      <Tabs.Screen name="ideas" options={{ title: 'Ideas', tabBarIcon: ({ color }) => <Ionicons name="bulb" size={24} color={color} /> }} />
+      <Tabs.Screen name="index.tsx" options={{ title: 'Inicio', tabBarIcon: ({ color }) => <Ionicons name="home" size={24} color={color} /> }} />
+      <Tabs.Screen name="notas/index.tsx" options={{ title: 'Notas', tabBarIcon: ({ color }) => <Ionicons name="document-text" size={24} color={color} /> }} />
+      <Tabs.Screen name="checklists/index.tsx" options={{ title: 'Checklist', tabBarIcon: ({ color }) => <Ionicons name="checkbox" size={24} color={color} /> }} />
+      <Tabs.Screen name="ideas/index.tsx" options={{ title: 'Ideas', tabBarIcon: ({ color }) => <Ionicons name="bulb" size={24} color={color} /> }} />
     </Tabs>
   );
 }
